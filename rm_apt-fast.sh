f6288bc43256553082c3d33eafa3bbe1bc4e17ad
nobodyxu <Jiahao_XU@outlook.com>
2020-02-23 11:38:35 +1100

    Hardcode path to rm_apt-fast.sh in rm_apt-fast.sh
    instead of using `$0`, which is problematic if user does not call
    rm_apt-fast.sh with the full path.

--- rm_apt-fast.sh
+++ rm_apt-fast.sh
@@ -3,4 +3,4 @@
 apt-get purge -y aria2
 apt-get autoremove -y
 
-exec rm /usr/local/sbin/apt-fast /usr/local/sbin/apt-auto /usr/local/etc/apt-fast.conf $0
+exec rm /usr/local/sbin/apt-fast /usr/local/sbin/apt-auto /usr/local/etc/apt-fast.conf /usr/local/sbin/rm_apt-fast.sh
